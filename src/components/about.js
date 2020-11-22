c8943fc5c5d7567c9e1a244229cd62411718f689
Melissa Vallery <vallery.melissa@gmail.com>
2020-11-22 07:47:01 -0600

    Resizing issues fixed and added about me

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,13 @@
+import Jason from '../assets/images/jason.png';
 function About() {
     return (
-<h1>This is about me page</h1>
-        
+        <div>
+<h1>Jason M. Vallery</h1>
+<img className="jason" src={Jason} align="right"></img>
+<p className="paragraph">This is a paragraph about Jason. He is the coolest of them all. He knows how to play viddy, and how to write a booky book.
+    Sometimes he is a lame-o but sometimes he's the okayest.
+</p>
+</div>
     );
   }
   
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,19 @@
-import BookCover from '../assets/images/bookcover.jpg'
+import {Router, Route, Link, Switch } from "react-router-dom";
+import BookCover from '../assets/images/cover-header.png';
+import ArborealPath from './arboreal-path';
+
 
 function Home() {
     return (
-        <div>
-        <h1 className="hHome">Arboreal Path</h1>  
-        <img className="Book-cover" src={BookCover} align="right"></img>
-        <p className="pHome">Dapibus ultrices in iaculis nunc sed. Hendrerit gravida rutrum quisque non tellus orci ac auctor. Rutrum quisque non tellus orci ac auctor. 
+        <div className= "paragraph-container">
+          <div flex-content>
+        <h1>Arboreal Path</h1>  
+        <div align="right" className="book-flex">
+        {/* <p className= "book-banner">Coming Soon!</p> */}
+        <img className="book-cover" src={BookCover} align="right"></img>
+        </div>
+        
+        <p className="paragraph">Dapibus ultrices in iaculis nunc sed. Hendrerit gravida rutrum quisque non tellus orci ac auctor. Rutrum quisque non tellus orci ac auctor. 
         Ut eu sem integer vitae justo eget magna fermentum. Odio tempor orci dapibus ultrices. Tempus iaculis urna id volutpat lacus. Massa tempor nec feugiat nisl pretium fusce id velit. <br/><br/>
         Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Augue ut lectus arcu bibendum at varius vel pharetra vel. Eget gravida cum sociis natoque penatibus et magnis.
 
@@ -13,7 +21,16 @@ function Home() {
         Sit amet purus gravida quis blandit. Cum sociis natoque penatibus et magnis. Pretium fusce id velit ut tortor pretium viverra. Est lorem ipsum dolor sit amet consectetur adipiscing elit. <br/><br/>
         Dui sapien eget mi proin sed. At tempor commodo ullamcorper a lacus vestibulum sed. Urna id volutpat lacus laoreet non curabitur gravida arcu. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet. Quis blandit turpis cursus in hac. Blandit libero volutpat sed cras ornare arcu dui vivamus. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Laoreet sit amet cursus sit amet. Consectetur a erat nam at lectus. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Nunc eget lorem dolor sed viverra ipsum.<br/><br/>
         
-</p>    </div>);
+        <Link className="linkAP" to="/arboreal-path">Learn more about Arboreal Path</Link>
+        </p>
+        </div>
+        <main>
+        <Switch>
+        <Route path="/arboreal-path" component= {ArborealPath} />
+
+        </Switch>
+        </main>
+    </div>);
   }
   
   export default Home;
