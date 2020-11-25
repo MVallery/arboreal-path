2feee19908a40b9e111989c151575113a614ca4a
Melissa Vallery <vallery.melissa@gmail.com>
2020-11-25 12:04:46 -0600

    border

--- src/components/arboreal-path.js
+++ src/components/arboreal-path.js
@@ -1,6 +1,7 @@
 import Hali from '../assets/images/hali.jpg';
 import Alim from '../assets/images/alim.jpg';
 import Tio from '../assets/images/tio.jpg';
+import {CSSTransition} from 'react-transition-group';
 
 import Map from '../assets/images/map.jpg';
 import React, { useState } from "react";
@@ -17,7 +18,7 @@ function ArborealPath() {
     const onCharacterClick = (name) => {
        if (name === "Hali") {
             setHaliSelect(["Hali's Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",1]);
-            setAlimSelect(["", 0.2]);
+            setAlimSelect(["", 0.2, false]);
             setTioSelect(["", 0.2]);
             setMapSelect(["", 0.2]);
         //     let haliCSS = document.querySelectorAll("hali");
@@ -31,8 +32,13 @@ function ArborealPath() {
         //     alimCSS.style.backgroundColor = Alim[1];
         }
         if (name === "Tio") {
-            setTioSelect(["Tio's Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",1]);
-            setAlimSelect(["", 0.2]);
+            setTioSelect(["Tio's Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" +
+            "minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea" +
+            "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
+            "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non" +
+            "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",1]);
+            setAlimSelect(["", 0.2, false]);
             setHaliSelect(["", 0.2]);
             setMapSelect(["", 0.2]);
             // let haliCSS = document.querySelectorAll("hali");
@@ -45,7 +51,12 @@ function ArborealPath() {
             // alimCSS.style.backgroundColor = Alim[1];
         }
         if (name === "Alim") {
-            setAlimSelect(["Alim's Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 1]);
+            setAlimSelect(["Alim's Lorem ipsum dolor sit amet, consectetur adipiscing elit," + 
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" + 
+            "minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea" +
+            "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
+            "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident," + 
+            "sunt in culpa qui officia deserunt mollit anim id est laborum.", 1, true]);
             setHaliSelect(["", 0.2]);
             setTioSelect(["", 0.2]);
             setMapSelect("");
@@ -69,14 +80,26 @@ function ArborealPath() {
         };
         
     };
-
+    // (function () {
+    //     var alim = document.getElementById('alim');
+    
+    //     alim.onclick = function () {
+    
+    //         if (alim.style.height === "450px") {
+    //             alim.style.height = "1000px";
+    
+    //         } else {
+    //             alim.style.height = "1000px";
+    //         }
+    //     };
+    // })();
     return (
         <div><h1 style={{textAlign:"center"}}>Arboreal Path</h1>
         <div className="ap-container">
         {/* {document.getElementById('character-container').scrollIntoView()} */}
 
             <div style={{opacity:haliSelect[1]}} >
-                <div className="character-container hali">
+                <div className="character-container" id="hali">
             <h1>Hali</h1>
             <a onClick= {() => {onCharacterClick("Hali");}}>
             <img className="character-content" src={Hali}></img></a>
@@ -95,13 +118,17 @@ function ArborealPath() {
             </div>
 
             <div style={{opacity:alimSelect[1]}}>
-            <div className="character-container alim">
+            <div className="character-container" id="alim">
 
             <h1>Alim</h1>
             <a onClick= {() => {onCharacterClick("Alim");}}>
 
             <img className="character-content" src={Alim}></img></a>
+            <CSSTransition in={alimSelect[2]} timeout={300} classNames="alim">
+                <view>
             {alimSelect[0]}
+            </view>
+            </CSSTransition>
             </div>
             </div>
 {/* 
@@ -116,7 +143,20 @@ function ArborealPath() {
         </div>     
     );
   }
-
+{/* <CSSTransition
+  in={this.state.display}
+  timeout={350}
+  classNames="display"
+  unmountOnExit
+>
+  <div className="menu">
+    <ul className="list">
+      <li className="list-item">Rajat</li>
+      <li className="list-item">Writes about React</li>
+      <li className="list-item">Loves Pizza</li>
+    </ul>
+  </div>
+</CSSTransition> */}
 
   export default ArborealPath;
 
