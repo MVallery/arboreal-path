9e0c0610ec1fba8daba7eb9aad5a27925aa5b500
Melissa Vallery <mvallery88@gmail.com>
2021-05-09 17:54:32 -0500

    restyle character page

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -12,15 +12,15 @@ const ArborealPath = props => {
     const [alimSelect, setAlimSelect] = useState([])
     const [mapSelect, setMapSelect] = useState("")
     const [characterList, setCharacterList] = useState([])
+    const [backgroundStyle, setBackgroundStyle] = useState('tio-background-color')
 
 
     const onCharacterClick = (name) => {
        if (name === "Hali") {
             setHaliSelect(["Hali's Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",0.85]);
-            setAlimSelect(["", 0.5, false]);
-            setTioSelect(["", 0.5]);
-            setMapSelect(["", 0.5]);
-
+            setAlimSelect(["", 0, false]);
+            setTioSelect(["", 0.]);
+            setBackgroundStyle('hali-background-color')
         }
         if (name === "Tio") {
             setTioSelect(["Tio's Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
@@ -29,9 +29,9 @@ const ArborealPath = props => {
             "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
             "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non" +
             "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",0.85]);
-            setAlimSelect(["", 0.5, false]);
-            setHaliSelect(["", 0.5]);
-            setMapSelect(["", 0.5]);
+            setAlimSelect(["", 0, false]);
+            setHaliSelect(["", 0]);
+            setBackgroundStyle('tio-background-color');
 
         }
         if (name === "Alim") {
@@ -41,20 +41,17 @@ const ArborealPath = props => {
             "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse" +
             "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident," + 
             "sunt in culpa qui officia deserunt mollit anim id est laborum.", 0.85, true]);
-            setHaliSelect(["", 0.5]);
-            setTioSelect(["", 0.5]);
-            setMapSelect("");
+            setHaliSelect(["", 0]);
+            setTioSelect(["", 0]);
+            setBackgroundStyle('alim-background-color')
+
 
         }
-        if (name === "Map") {
-            setMapSelect("World Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-            setAlimSelect("");
-            setTioSelect("");
-            setHaliSelect("");
 
-        };
         
     };
+
+
     const haliStyle = haliSelect&&haliSelect[0] ? {height:'300px', display:'flex'}: {height:'0px', opacity:'0'}
     const tioStyle = tioSelect&&tioSelect[0] ? {height:'300px', display:'flex'}: {height:'0px', opacity:'0'}
     const alimStyle = alimSelect&&alimSelect[0] ? {height:'300px', display:'flex'}: {height:'0px', opacity:'0'}
@@ -66,15 +63,21 @@ const ArborealPath = props => {
     const haliPictureGlow = haliSelect && haliSelect[0] ? {backgroundImage: 'radial-gradient(black, purple, rgba(128, 0, 128, 0.36), transparent)'}:null;
     const tioPictureGlow = tioSelect && tioSelect[0] ? {backgroundImage: 'radial-gradient(green, rgba(55, 154, 53, 0))'}:null;
     const alimPictureGlow = alimSelect && alimSelect[0] ? {backgroundImage: 'radial-gradient(#e00b9e, transparent)'}:null
+
+
+    const haliContainerStyle = haliSelect&&haliSelect[0] ? 'hali-container-style-active': 'hali-container-style-inactive'
+    const alimContainerStyle = alimSelect&&alimSelect[0] ? 'alim-container-style-active': 'alim-container-style-inactive'
+    const tioContainerStyle = tioSelect&&tioSelect[0] ? 'tio-container-style-active': 'tio-container-style-inactive'
+
     return (
         
         <div className="characters-main-container" id='characters' ref={props.arborealPathRef}>
         {/* <h1 style={{textAlign:"center"}}>Characters</h1> */}
         
-        <div className="characters-container">
+        <div className={`characters-container ${backgroundStyle}`}>
             
             <div onClick= {() => {onCharacterClick("Hali");}}  >
-                <div className="character-container hali" style={haliBackgroundStyle}>
+                <div className={`character-container hali ${haliContainerStyle}`}>
             <h1>Hali</h1>
             <a onClick= {() => {onCharacterClick("Hali");}}>
             <img className="character-content" style={{...haliPictureGlow, opacity:haliSelect[1]}} src={Hali}></img></a>
@@ -85,7 +88,7 @@ const ArborealPath = props => {
             </div>
 
             <div onClick={() => {onCharacterClick("Tio");}}>
-            <div className="character-container tio" style={tioBackgroundStyle}>
+            <div className={`character-container tio ${tioContainerStyle}`}>
 
             <h1>Tio</h1>
             <a onClick= {() => {onCharacterClick("Tio");}}>
@@ -97,7 +100,7 @@ const ArborealPath = props => {
             </div>
 
             <div onClick={() => {onCharacterClick("Alim");}} >
-            <div className="character-container alim" style={alimBackgroundStyle}>
+            <div className={`character-container alim ${alimContainerStyle}`} >
 
             <h1>Alim</h1>
             <a onClick= {() => {onCharacterClick("Alim");}}>
