44c150df8330dfb552ee2c7265f7537b56da8fac
Melissa Vallery <mvallery88@gmail.com>
2021-05-22 15:15:41 -0500

    Added 'other' dropdown to home page and compressed background images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Footer = () => {
   return(
-  <p className= "footer">Copyright © 2020 |  Arcane Moose Production</p>
+  <p className= "footer">Copyright © 2021 |  Jason M. Vallery</p>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,41 @@ import React from "react";
 import { Router, Route, Link, Switch } from "react-router-dom";
 import BookCover from "../assets/images/bookcover.jpg";
 import ArborealPath from "./Characters";
+import { withStyles } from '@material-ui/core/styles';
+
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 import { Link as RSLink } from "react-scroll";
 
 const Home = (props) => {
+  const [retailMenu, setRetailMenu] = React.useState(false)
+  const handleShowRetailMenu = (e) => {
+    e.preventDefault()
+    console.log(e.currentTarget)
+    setRetailMenu(e.currentTarget)
+  }
+  const handleCloseRetailMenu = () => {
+    setRetailMenu(false)
+  }
+  const StyledMenu = withStyles({
+    paper: {
+      border: '1px solid #d3d4d5',
+    },
+  })((props) => (
+    <Menu
+      elevation={0}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      {...props}
+    />
+  ));
   return (
     <div id="home" ref={props.homeRef} className="home-container">
       <div className="home-gradient">
@@ -27,7 +59,7 @@ const Home = (props) => {
           <div className="home-book-title">
             <h1> Arboreal Path </h1>
 
-            <p>
+            
               {/* The cost of magic is never cheap.<br/><br/>
 
 Something Hali is all too familiar with. Renowned as a hero in her country, Hali had been seen as their only hope as a plague tore through the populace. Though her spell succeeded in curing the affliction, it had taken her unborn son in the process and created a monstrous shadow. Imprisoning the shadow inside a magical tattoo on her arm, Hali couldn’t contain its power before it killed her wife, adding another face to its collection.
@@ -37,33 +69,37 @@ With her family unable to find peace, Hali has a single goal—set them free. Bu
 Alim—a former explorer of forgotten ruins who was born magicless—is roped into Hali’s plans with the promise of attaining the power of the stars.<br/>
 As one of the few remaining with nature magic, Tio may hold the key to breaking into the realm of mystical beasts and spirits to unlock Alim’s magic.<br/>
 As the three journeys collide, the bonds of their new friendship are tested as the world reminds them of the cost of magic.<br/> */}
-              Magic is disappearing.
-              <br />
-              Technology is rising.
-              <br />
-              <br />
-              Hali—a scholar of ancient magic—seeks to free her dead son and
-              wife from the shadow monster trapped in her tattoo by any means
-              necessary.
-              <br />
-              Alim—a former explorer of forgotten ruins who was born
-              magicless—is roped into Hali’s plans with the promise of attaining
-              the power of the stars.
-              <br />
-              Tio—one of the few remaining with nature magic—discovers the
-              source of magic is dying and now must juggle his social anxieties
-              with his mission as he creates a team to prevent it.
-              <br />
-              After a chance encounter brings the three together, their shared
-              goal becomes clear. Break into the realm of mystical beasts and
-              spirits.
-            </p>
+<p>The cost of magic is never cheap.</p><br/>
+
+<p>Three unlikely friends collide in a quest to restore the world's dwindling magic.</p>
+            
             <h2> Preorder Now! </h2>
             <div className="home-buy-now-container">
               <button>AMAZON</button>
               <button>BARNES & NOBLE</button>
-              <button>BARNES & NOBLE</button>
+              <button onClick={handleShowRetailMenu}>OTHER RETAILERS</button>
             </div>
+            <StyledMenu
+              disableScrollLock = {true}
+              id="retail-menu"
+              anchorEl={retailMenu}
+              style={{position:'absolute'}}
+              keepMounted
+              open={Boolean(retailMenu)}
+              onClose={handleCloseRetailMenu}
+              getContentAnchorEl={null}
+            >
+              <MenuItem 
+                onMouseEnter={(e) => {e.target.style.backgroundColor = '#d7d7d8'; e.target.style.color='white'}}
+                onMouseLeave={(e) => {e.target.style.backgroundColor = 'white'; e.target.style.color='black'}}
+                >Other site 1</MenuItem>
+              <MenuItem>
+                Other site 2
+              </MenuItem>
+              <MenuItem>Other site 3</MenuItem>
+
+              <MenuItem>Other site 4</MenuItem>
+            </StyledMenu>
 
             {/* <Link to="/arboreal-path">
                 Explore the characters
