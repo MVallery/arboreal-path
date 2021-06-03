95f1aeebbf7921983ff97abec16dc21671682596
Melissa Vallery <mvallery88@gmail.com>
2021-06-02 23:19:46 -0500

    Restructured Characters component to ensure transitions worked appropriately
    Issues with transitions on the left click/ previously it was either not working or transitioned to the wrong character. Set up each character card as a state that can be toggled active/inactive with the onCharacterClick function

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,72 +1,53 @@
 import React from 'react';
 
 const CharacterCard = (props) => {
-  if (props.index === 1) {
-    return(
-      <div
-        className={`character-container ${props.cardBackground} ${props.containerStyle}`} 
-      >
-      <a className="prev" onClick={ () => {props.onLeftClick()}}><p className="arrow-name">{props.characterList[0].name}</p>&#10094;</a>
-      <a className="next" onClick={ () => {props.onRightClick()}}><p className="arrow-name">{props.characterList[2].name}</p>&#10095;</a>
+  let inactive
+
+  let left;
+  let right;
+  if (props.character.name==='Tio'){
+    left = 'Alim'
+    right = 'Hali'
+  } else if (props.character.name==='Hali'){
+    left = 'Tio'
+    right = 'Alim'
+  } else {
+    left = 'Hali'
+    right = 'Tio'
+  }
+  return(
+      <div key={props.character.name} className={`character-container ${props.cardBackground} ${props.containerStyle}`} >
+        <a className="prev" onClick={ () => {props.onCharacterClick(left)}}><p className="arrow-name">{left}</p>&#10094;</a>
+        <a className="next" onClick={ () => {props.onCharacterClick(right)}}><p className="arrow-name">{right}</p>&#10095;</a>
         <div className="character-img-description-container">
           <div className="flex-column image-name-container">
-        <h1>{props.character.name}</h1>
-        <a>
-          <img
-            className="character-content"
-            style={{ ...props.pictureGlow,  opacity:0.65}}
-            src={props.character.image}
-            alt='character picture'
-          ></img>
-        </a>
-        </div>
+            <h1>{props.character.name}</h1>
+            <a>
+              <img
+                className="character-img"
+                style={{ ...props.pictureGlow,  opacity:0.65}}
+                src={props.character.image}
+                alt='character picture'
+              ></img>
+            </a>
+          </div>
 
-        <div
-          id="expanded-text"
-          className={`character-description ${props.characterDescription}`}
-          style={props.characterStyle}
-        >
-          <span className='character-quote'>
-            <span className='quotation-marks'>“</span>
-          {props.character.quote}
-          <span className='quotation-marks'>”</span>
-          <br/><br/>
-          </span>
-          {props.character.description}
+          <div
+            id="expanded-text"
+            className={`character-description ${props.characterDescription}`}
+            style={props.characterStyle}
+          >
+            <span className='character-quote'>
+              <span className='quotation-marks'>“</span>
+                {props.character.quote}
+              <span className='quotation-marks'>”</span>
+            <br/><br/>
+            </span>
+              {props.character.description}
+          </div>
         </div>
-        </div>
-
       </div>
-    ) 
-  } else {
-    return(
-    <div className={`character-container inactive-style ${props.cardBackground} ${props.containerStyle}`} >
-            <a className="prev" onClick={ () => {props.onLeftClick()}}><p className="arrow-name">{props.characterList[0].name}</p>&#10094;</a>
-      <a className="next" onClick={ () => {props.onRightClick()}}><p className="arrow-name">{props.characterList[2].name}</p>&#10095;</a>
-        <h1>{props.character.name}</h1>
-        <a>
-          <img
-            className="character-content"
-            style={{ ...props.pictureGlow, opacity: 0.65 }}
-            src={props.character.image}
-          ></img>
-        </a>
-        <div
-          id="expanded-text"
-          className={`character-description ${props.characterDescription}`}
-          style={props.characterStyle}
-        >
-          <span className='character-quote'>
-          <span className='quotation-marks'>“</span>
-          {props.character.quote}
-          <span className='quotation-marks'>”</span>
-          </span>
-          {props.character.description}
-
-        </div>
-    </div>
     )
-  }
 }
 
 export default CharacterCard;
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -27,61 +27,64 @@ const ArborealPath = props => {
             quote:` Don't like crowds? Let me be your bulwark. Point me in the right direction and I'll cut a path for you.`,
             image:Alim}]);
 
-
     const [backgroundStyle, setBackgroundStyle] = useState('hali-background-color');
-    const onLeftClick = () => {
-        onRightClick();
-        onRightClick();
-    }
-    const onRightClick = () => {
-        const temp = JSON.parse(JSON.stringify(characterList));
-        const firstElement = temp.shift();
-        temp.push(firstElement)
-        setCharacterList(temp)
-        let newBackgroundstyle = temp[1].name==='Tio'? 'tio-background-color' : temp[1].name==='Alim'? 'alim-background-color'  : 'hali-background-color'
-        setBackgroundStyle(newBackgroundstyle)
-
-    }
-
-    const displayCharacterCards = characterList.map((character, index)=> {
-        let pictureGlow;
-        let cardBackground;
-        let characterDescription;
-
-        if (character.name==='Tio'){
-            pictureGlow = {backgroundImage: 'radial-gradient(#5dd85d, #137b2a00, rgba(55, 154, 53, 0))'};
-            cardBackground = 'tio-card-background'
-            characterDescription = 'character-description-tio'
-        } else if (character.name==='Alim'){
-            pictureGlow = {backgroundImage: 'radial-gradient(#e0dd15, rgba(232, 23, 226, 0.08), transparent)'}
-            cardBackground = 'alim-card-background'
-            characterDescription = 'character-description-alim'
-        } else {
-            pictureGlow = {backgroundImage: 'radial-gradient(black, #80007f, rgba(128, 0, 128, 0), transparent)'};
-            cardBackground = 'hali-card-background'
-            characterDescription = 'character-description-hali'
+    const [tioCardBackground, setTioCardBackground] = useState('inactive-style')
+    const [alimCardBackground, setAlimCardBackground] = useState('inactive-style')
+    const [haliCardBackground, setHaliCardBackground] = useState('hali-card-background')
+    
+    const onCharacterClick = (character) => {
+        switch (character){
+            case 'Tio':
+                setTioCardBackground('tio-card-background')
+                setAlimCardBackground('inactive-style')
+                setHaliCardBackground('inactive-style')
+                setBackgroundStyle('tio-background-color')
+                break;
+            case 'Alim':
+                setAlimCardBackground('alim-card-background')
+                setTioCardBackground('inactive-style')
+                setHaliCardBackground('inactive-style')
+                setBackgroundStyle('alim-background-color')
+                break;
+            default:
+                setHaliCardBackground('hali-card-background')
+                setAlimCardBackground('inactive-style')
+                setTioCardBackground('inactive-style')
+                setBackgroundStyle('hali-background-color')
+                break;
+            
         }
-        return (
-            <CharacterCard 
-                key={character.name} 
-                characterList={characterList} 
-                characterDescription={characterDescription} 
-                character={character} 
-                cardBackground={cardBackground} 
-                pictureGlow ={pictureGlow} 
-                index={index} 
-                onRightClick={onRightClick} 
-                onLeftClick={onLeftClick}/>
-        )
-    }) 
+    }
+
     return (
         <React.Fragment>
             <div className="characters-fade-container" id='characters' ref={props.arborealPathRef}>
-            <div className="characters-main-container">
-                <div className={`characters-container ${backgroundStyle}`}> 
-                    {displayCharacterCards}
+                <div className="characters-main-container">
+                    <div className={`characters-container ${backgroundStyle}`}> 
+                        {/* {displayCharacterCards} */}
+                        <CharacterCard //Tio
+                            characterDescription='character-description-tio'
+                            character={characterList[0]} 
+                            cardBackground= {tioCardBackground}
+                            pictureGlow ={{backgroundImage: 'radial-gradient(#5dd85d, #137b2a00, rgba(55, 154, 53, 0))'}}
+                            onCharacterClick={onCharacterClick}
+                            />
+                        <CharacterCard //Hali
+                            characterDescription= 'character-description-hali'
+                            character={characterList[1]} 
+                            cardBackground={haliCardBackground}
+                            pictureGlow = {{backgroundImage: 'radial-gradient(black, #80007f, rgba(128, 0, 128, 0), transparent)'}}
+                            onCharacterClick={onCharacterClick}
+                            />
+                        <CharacterCard //Alim
+                            characterDescription= 'character-description-alim'
+                            character={characterList[2]} 
+                            cardBackground={alimCardBackground}
+                            pictureGlow ={{backgroundImage: 'radial-gradient(#e0dd15, rgba(232, 23, 226, 0.08), transparent)'}} 
+                            onCharacterClick={onCharacterClick}
+                            />
+                    </div>
                 </div>
-            </div>
             </div>
         </React.Fragment>
     );
